13e9bb96eb24a2e763181e8f4a065b57fe7ca121
Özgür Korkmaz <ozgurkorkmaz236@gmail.com>
2025-04-17 16:38:54 +0300

    commit

--- js/task-1.js
+++ js/task-1.js
@@ -1,9 +1,9 @@
 function makeTransaction(quantity, pricePerDroid, customerCredits){
     const totalPrice = quantity * pricePerDroid;
     if(totalPrice> customerCredits){
-        console.log("Insufficient funds!");
+        return "Insufficient funds!" ;
     }else{
-        console.log(`You ordered ${quantity} droids worth ${totalPrice} credits!"`)
+        return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
     }
 }
 
